305bac3bca570bf052c83fca38511cf3fbf1fc17
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-12-18 17:37:19 +0100

    day 1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -11,3 +11,16 @@ func CalculateIncreases(depthReport []int) int {
 
 	return increases
 }
+
+func CalculateIncreasesWithSlidingWindow(depthReport []int) int {
+	var slidingWindowTotals []int
+
+	for i := 1; i < len(depthReport)-1; i++ {
+		first := depthReport[i-1]
+		second := depthReport[i]
+		third := depthReport[i+1]
+		slidingWindowTotals = append(slidingWindowTotals, first+second+third)
+	}
+
+	return CalculateIncreases(slidingWindowTotals)
+}
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -6,14 +6,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Day1ExampleHas7Increases(t *testing.T) {
+func Test_Day1Part1ExampleHas7Increases(t *testing.T) {
 	reports := ReadReports("./fixtures/example.txt")
 	increases := CalculateIncreases(reports)
 	assert.Equal(t, 7, increases)
 }
 
 func Test_Part1(t *testing.T) {
-	reports := ReadReports("./fixtures/part1.txt")
+	reports := ReadReports("./fixtures/day1.txt")
 	increases := CalculateIncreases(reports)
 	assert.Equal(t, 1390, increases)
+}
+
+func Test_Day1Part2ExampleHas5IncreasesWithSlidingWindow(t *testing.T) {
+	reports := ReadReports("./fixtures/example.txt")
+	increases := CalculateIncreasesWithSlidingWindow(reports)
+	assert.Equal(t, 5, increases)
+}
+
+func TestPart2(t *testing.T) {
+	reports := ReadReports("./fixtures/day1.txt")
+	increases := CalculateIncreasesWithSlidingWindow(reports)
+	assert.Equal(t, 1457, increases)
 }
